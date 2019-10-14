adf116ff1bfb1bdce04a07bb77573f5a55e88e37
giraffebot <girafbot@gmail.com>
2019-10-13 20:55:51 -0400

    made header.php and jslibraries.php files, used appropriately across pages

--- upload.php
+++ upload.php
@@ -1,44 +1,34 @@
 <?php
 $target_dir = "/var/www/html/students/jnijs/uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$upload_ok = 1;
+$upload_ok = $_POST["upload_ok"];
 
-// check if the file exists. we can have an option to ignore this on the form
-if (file_exists($target_file)) {
-    echo "the file already exists on this server";
-    $upload_ok = 0;
-}
+// uncomment below if you want it/want to ask the admin if they really want to replace the file, but i suspect that we won't need it
 
-// check file size. again, not necessary but options are good
-if ($_FILES["to_upload"]["size"] > 500000) {
-    echo "your file is too large";
-    $upload_ok = 0;
-}
+// // check if the file exists. we can have an option to ignore this on the form
+// if (file_exists($target_file)) {
+//     echo "the file already exists on this server\n";
+//     $upload_ok = 2;
+// }
 
+// we dont need to restrict ourselves to this either!!
 if (! in_array(pathinfo($_FILES["to_upload"]["name"], PATHINFO_EXTENSION), [
 	0 => "doc",
 	1 => "docx",
 	2 => "pdf",
 ])) {
-	echo "invalid file format";
-	$upload_ok = 0;
+	$upload_ok = 3;
 }
 
 // check if $upload_ok is set to 0 by an error
-if ($upload_ok == 0) {
-    echo "file was not uploaded";
+if (move_uploaded_file($_FILES["to_upload"]["tmp_name"], $target_file)) {
+        $upload_ok = 1;
 } else {
-    if (move_uploaded_file($_FILES["to_upload"]["tmp_name"], $target_file)) {
-        echo "file " . basename( $_FILES["to_upload"]["name"]) . " has been uploaded";
-    } else {
-        echo "sorry, there was an error uploading your file";
-    }
+    $upload_ok = 0; // unknown error
 }
 
-echo "debug:<pre>";
-echo "\nuploaded to: ";
-print_r("\n" . $target_file . "\n");
-print_r($_FILES);
-print "</pre>";
-
+// redirect to original page
+// upload_ok doesnt matter outside the realm of error checking on the page; it's not a security issue
+// alternatively we can have the action redirect to the location of the file on-server if that seems to be a better option, but this should be fine for now
+header("Location: " . $_SERVER['HTTP_REFERER'] . "?upload_ok=" . $upload_ok);
 ?>
