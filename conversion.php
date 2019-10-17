980319c4a6649e7cb57eba89658af85c27c754c3
Matt Stauffer <mattstauffer20@gmail.com>
2019-10-17 13:44:47 -0400

    Edited Conversion Script
    All that we need is to create a variable for the file name. We also need to find a location to save the files.

--- conversion.php
+++ conversion.php
@@ -4,14 +4,14 @@ $word = new COM("Word.Application") or die ("Could not initialise Object.");
 $word->Visible = 0;
 // recommend to set to 0, disables alerts like "Do you want MS Word to be the default .. etc"
 $word->DisplayAlerts = 0;
-// open the word 2007-2013 document 
-$word->Documents->Open('yourdocument.docx');
+// open the word 2007-2013 document
+$word->Documents->Open($INSERT_FILENAMEHERE);
 // save it as word 2003
-$word->ActiveDocument->SaveAs('newdocument.doc');
+$word->ActiveDocument->SaveAs($INSERT_FILENAMEHERE);
 // convert word 2007-2013 to PDF
-$word->ActiveDocument->ExportAsFixedFormat('yourdocument.pdf', 17, false, 0, 0, 0, 0, 7, true, true, 2, true, true, false);
+$word->ActiveDocument->ExportAsFixedFormat($INSERT_FILENAMEHERE .".pdf", 17, false, 0, 0, 0, 0, 7, true, true, 2, true, true, false);
 // quit the Word process
 $word->Quit(false);
 // clean up
-unset($word);
+$word=null;
 ?>
