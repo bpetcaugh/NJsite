5e41a4ec072965c817c21bd1695d02b11b3b0475
giraffebot <girafbot@gmail.com>
2019-10-22 11:42:58 -0400

    drag-and-drop uploads halfway implemented

--- NJPage.js
+++ NJPage.js
@@ -8,7 +8,47 @@ $(document).ready(function () {
         $('#sidebar').toggleClass('active');
         $(this).toggleClass('active');
         if($(window).height() <= 675) { // Toggle elements on mobile
-            $(".headerLogo").toggle();  
+            $(".headerLogo").toggle();
         }
     });
+
+	$("div.upload").each((i, elem) => {
+		elem.id = i.toString();
+
+		["dragenter", "dropenter", "dragleave", "drop"].forEach(eventName => {
+			elem.on(eventName, () => {
+				e.preventDefault();
+				e.stopPropagation();
+			});
+		});
+
+		["dragenter", "dragover"].forEach(eventName => {
+			elem.on(eventName, e => {
+				e.addClass("upload-highlight");
+			});
+		});
+
+		["dragleave", "drop"].forEach(eventName => {
+			elem.on(eventName, e => {
+				e.removeClass("upload-highlight");
+			});
+		});
+
+		elem.on("drop", e => {
+			e.dataTransfer.files.forEach(file => {
+				let formData = new FormData();
+
+				formData.append("file", file);
+
+				fetch("", {
+					method: "POST",
+					body: formData
+				}).then(() => {
+					// upload successful
+				}).catch(() => {
+					// upload failed
+				});
+			});
+		});
+	});
 });
