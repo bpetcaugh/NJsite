36bf48b22f0c0e885597b7fa1fb989d3aba25d73
giraffebot <girafbot@gmail.com>
2019-10-30 16:30:35 -0400

    made sidebar. needs to change on a page-by-page basis

---
+++ includes/sidebar.php
@@ -0,0 +1,34 @@
+<nav id="sidebar">
+	<div class="sidebar-header">
+		<table>
+			<tr>
+				<td width="25%">
+					<a href="NJadminpage.html"><img src="icon-login.png" width="45px"></a>
+				</td>
+				<td width="75%" style="padding-left: 15px;">
+					<h3>Navigation</h3>
+				</td>
+			</tr>
+		</table>
+	</div>
+	<ul class="list-unstyled components">
+		<li>
+			<a href="https://www.nj.gov/dcf/">DCF Home</a>
+		</li>
+		<li>
+			<a href="https://www.nj.gov/dcf/families/">Families</a>
+		</li>
+		<li>
+			<a href="https://www.nj.gov/dcf/adolescent/">Adolescents</a>
+		</li>
+		<li>
+			<a href="https://www.nj.gov/dcf/women/">Women</a>
+		</li>
+		<li>
+			<a href="https://www.nj.gov/dcf/providers/">Providers & Stakeholders</a>
+		</li>
+		<li>
+			<a href="https://www.nj.gov/dcf/about/divisions/oa/">Advocacy</a>
+		</li>
+	</ul>
+</nav>
