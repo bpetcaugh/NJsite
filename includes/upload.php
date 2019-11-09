9d635a3b663d571f495f2d9862467fef76558b6d
giraffebot <girafbot@gmail.com>
2019-11-09 17:59:17 -0500

    gonna test uploads

--- includes/upload.php
+++ includes/upload.php
@@ -1,11 +1,20 @@
+<!--
+	HEY MATT YOU GOTTA PUT THE FILE NAME AND UPLOAD REASON INTO THE DATABASE IN HERE PLEASE DO THAT I HAVE NO IDEA HOW TO DO SQL THANKS
+
+	UPLOAD REASON IS IN $_POST["reason"]
+	NEW FILE NAME IS IN $_POST["newname"]
+
+	OK THANKS COOL DO THE SQL THING NICE YEA
+	YOUVE GOTTA FIGURE OUT HOW TO GET THE REASONS TO WORK WITH THE VERSION NUMBER THING YEA NICE AIGHT COOL GOOD LUCK
+-->
+
 <?php
 	$target_dir = "../res/temp/";
-	$target_file = $target_dir. basename($_FILES["to_upload"]["name"]);
-	$upload_ok = $_POST["upload_ok"];
-	if (move_uploaded_file($_FILES["to_upload"]["tmp_name"], $target_file)) {
-	    $upload_ok = 1;
+	$target_file = $target_dir . $_POST["newname"];
+	if (move_uploaded_file($_POST["to_upload"]["tmp_name"], $target_file)) {
+	    // things are good
 	} else {
-	    $upload_ok = 0; // unknown error
+	    // unknown error
 	}
 	$output = shell_exec('python3 ../res/py/upload.py');
 ?>
